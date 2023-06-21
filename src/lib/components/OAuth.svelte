5a60a95510f2b962adb42449e3f0c8ad8243e971
Ysn4Irix <80527916+Ysn4Irix@users.noreply.github.com>
2023-06-21 23:21:55 +0100

    :no_entry: disabling Google Auth (Waiting for production verification)

--- src/lib/components/OAuth.svelte
+++ src/lib/components/OAuth.svelte
@@ -5,6 +5,7 @@
 
 {#each providers.authProviders as provider}
 	<a
+		class:disabled={provider.name === 'google'}
 		class="m-1 inline-flex w-full items-center justify-center gap-2 rounded-md border bg-white px-4 py-3 align-middle text-sm font-medium text-gray-700 shadow-sm transition-all hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-slate-800 dark:hover:text-white dark:focus:ring-offset-gray-800"
 		href={`/oauth?provider=${provider.name}`}
 	>
